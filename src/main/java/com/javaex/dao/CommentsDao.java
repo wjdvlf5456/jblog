01c1121a190e734ac44d77c780ee1cc56164c274
최정필 <wjdvlf1407@gmail.com>
2022-07-07 17:21:11 +0900

    cmt DAO

--- src/main/java/com/javaex/dao/CommentsDao.java
+++ src/main/java/com/javaex/dao/CommentsDao.java
@@ -1,13 +1,38 @@
 package com.javaex.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.CommentsVo;
 
 @Repository
 public class CommentsDao {
 	
 	@Autowired
 	private SqlSession sqlSession;
+	
+	public List<CommentsVo> commentsList(){
+		List<CommentsVo> cmtList = sqlSession.selectList("cmt.selectcommentsList");
+		
+		return cmtList;
+	};
+	
+	public CommentsVo getComments(int cmtNo) {
+		CommentsVo cmtVo = sqlSession.selectOne("cmt.getCmt",cmtNo);
+		return cmtVo;
+	};
+	
+	public int cmtInsert(CommentsVo cmtVo) {
+		int count = sqlSession.insert("cmt.cmtInsert",cmtVo);
+		return count;
+	};
+	
+	public int cmtDelete(int cmtNo) {
+		int count = sqlSession.delete("cmt.cmtDelete",cmtNo);
+		return count;
+	};
 
 }
--- src/main/java/com/javaex/dao/PostDao.java
+++ src/main/java/com/javaex/dao/PostDao.java
@@ -1,13 +1,38 @@
 package com.javaex.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.PostVo;
 
 @Repository
 public class PostDao {
 	
 	@Autowired
 	private SqlSession sqlSession;
+	
+	public List<PostVo> postList(){
+		List<PostVo> pList = sqlSession.selectList("post.selectPostList");
+		
+		return pList;
+	};
+	
+	public PostVo getPost(int postNo) {
+		PostVo postVo = sqlSession.selectOne("post.getpost",postNo);
+		return postVo;
+	};
+	
+	public int postInsert(PostVo postVo) {
+		int count = sqlSession.insert("post.postInsert",postVo);
+		return count;
+	};
+	
+	public int postDelete(int postNo) {
+		int count = sqlSession.delete("post.postDelete",postNo);
+		return count;
+	};
 
 }
