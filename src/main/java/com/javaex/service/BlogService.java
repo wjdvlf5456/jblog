1815ac2067a4d0553d7331fddf3e29d20b0ecafd
최정필 <wjdvlf1407@gmail.com>
2022-07-11 14:46:45 +0900

    파일없이 blogTitle만 수정했을 시

--- src/main/java/com/javaex/service/BlogService.java
+++ src/main/java/com/javaex/service/BlogService.java
@@ -25,108 +25,121 @@ import com.javaex.vo.UsersVo;
 
 @Service
 public class BlogService {
-	
-	
+
 	@Autowired
 	private UsersDao usersDao;
-	
+
 	@Autowired
 	private BlogDao blogDao;
-	
+
 	@Autowired
 	private CategoryDao cateDao;
-	
+
 	@Autowired
 	private PostDao postDao;
-	
+
 	// ===================== 해당 id 블로그 접속 시 정보 호출 =====================
-	public Map<String,Object> getBlog(String id) {
+	public Map<String, Object> getBlog(String id) {
 		BlogVo blogVo = blogDao.getBlog(id);
 		UsersVo uVo = usersDao.selectUsersInfo(id);
 		List<CategoryVo> cList = cateDao.cateList(id);
 		System.out.println("cList:" + cList.toString());
-		
-		List<PostVo> pList= postDao.postList(cList.get(0).getCateNo());
+
+		List<PostVo> pList = postDao.postList(cList.get(0).getCateNo());
 		PostVo pVo = new PostVo();
-		if (pList.size()<1) {// 아직 포스트가 하나도 없을 때 오류 방지
-			
-		} else {	
+		if (pList.size() < 1) {// 아직 포스트가 하나도 없을 때 오류 방지
+
+		} else {
 			pVo = pList.get(0);
-			
+
 		}
 		System.out.println(pVo);
-		
-		//블로그 메인에 넣어줄 정보들 맵으로 넣기
-		Map<String,Object> pMap = new HashMap<String,Object>();
+
+		// 블로그 메인에 넣어줄 정보들 맵으로 넣기
+		Map<String, Object> pMap = new HashMap<String, Object>();
 		pMap.put("blogVo", blogVo);
 		pMap.put("userName", uVo.getUserName());
 		pMap.put("cList", cList);
-		pMap.put("pList",pList);
-		pMap.put("pVo",pVo);
-		
+		pMap.put("pList", pList);
+		pMap.put("pVo", pVo);
+
 		return pMap;
 	};
-	
-	// ===================== 관리자일 때 블로그 수정 =====================
-	public int blogUpdate(String id,String blogTitle,MultipartFile file) {
-		
-		String saveDir = "/Users/choijungphil/javaStudy/upload";
-		
-		//오리지널 파일명
-		String orgName = file.getOriginalFilename();
-		
-		//확장자명 가져오기
-		String exName = orgName.substring(orgName.lastIndexOf("."));
-		
-		//드라이브에 저장할 파일명
-		String saveName = "";
-		
-		BlogVo bVo = blogDao.getBlog(id);
-		if (orgName.equals("")) {
-			saveName = bVo.getLogoFile();
-		} else {
-			saveName = System.currentTimeMillis() + UUID.randomUUID().toString() + exName;
-		}
-		
-		System.out.println(saveName);
-		
-		//파일경로(디렉토리+저장파일명)
-		String filePath = saveDir+ "/"+ saveName;
-		
-		BlogVo blogVo = new BlogVo(id,blogTitle,saveName);
-		
-		//이전에 있던 사진은 삭제한다.
-		String prevFile = saveDir + "/" + bVo.getLogoFile();
-		
-		System.out.println(bVo.getLogoFile());
-		File deleteFile = new File(prevFile);
-		
-		//(1)다오로 보내서 DB 업데이트
-		int count = blogDao.blogUpdate(blogVo);
 
-		// 파일이 존재하는지 체크 존재할경우 true, 존재하지않을경우 false
-		if (deleteFile.exists()) {
-			// 파일을 삭제합니다.
-			deleteFile.delete();
-			System.out.println("파일을 삭제하였습니다.");
+	// ===================== 관리자일 때 블로그 수정 =====================
+	public int blogUpdate(String id, String blogTitle, MultipartFile file) {
+
+		String saveDir = "/Users/choijungphil/javaStudy/upload";
+		System.out.println("file: " + file);
+		String orgName = "";
+		String saveName = "";
+		int count = 0;
+
+		if (file.getOriginalFilename().equals("")) {
+			System.out.println("파일 미업로드시");
+			saveName =null;
+			BlogVo blogVo = new BlogVo(id, blogTitle, saveName);
+
+			// (1)다오로 보내서 DB 업데이트
+			count = blogDao.blogUpdate(blogVo);
+
 		} else {
-			System.out.println("파일이 존재하지 않습니다.");
+			// 오리지널 파일명
+			orgName = file.getOriginalFilename();
+			System.out.println("orgName: " + orgName);
+
+			// 확장자명 가져오기
+			String exName = orgName.substring(orgName.lastIndexOf("."));
+
+			// 드라이브에 저장할 파일명
+			saveName = "";
+
+			BlogVo bVo = blogDao.getBlog(id);
+			if (orgName.equals("")) {
+				saveName = bVo.getLogoFile();
+			} else {
+				saveName = System.currentTimeMillis() + UUID.randomUUID().toString() + exName;
+			}
+
+			System.out.println("saveName: " + saveName);
+
+			// 파일경로(디렉토리+저장파일명)
+			String filePath = saveDir + "/" + saveName;
+
+			BlogVo blogVo = new BlogVo(id, blogTitle, saveName);
+
+			// 이전에 있던 사진은 삭제한다.
+			String prevFile = saveDir + "/" + bVo.getLogoFile();
+
+			System.out.println(bVo.getLogoFile());
+			File deleteFile = new File(prevFile);
+
+			// (1)다오로 보내서 DB 업데이트
+			count = blogDao.blogUpdate(blogVo);
+
+			// 파일이 존재하는지 체크 존재할경우 true, 존재하지않을경우 false
+			if (deleteFile.exists()) {
+				// 파일을 삭제합니다.
+				deleteFile.delete();
+				System.out.println("파일을 삭제하였습니다.");
+			} else {
+				System.out.println("파일이 존재하지 않습니다.");
+			}
+
+			// (2)파일저장
+			try {
+				byte[] fileData = file.getBytes();
+				OutputStream os = new FileOutputStream(filePath);
+				BufferedOutputStream bos = new BufferedOutputStream(os);
+
+				bos.write(fileData);
+				bos.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		
-		//(2)파일저장
-		try {
-			byte[] fileData = file.getBytes();
-		OutputStream os = new FileOutputStream(filePath);
-		BufferedOutputStream bos = new BufferedOutputStream(os);
-		
-		bos.write(fileData);
-		bos.close();
-		
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+
 		return count;
 	};
 
